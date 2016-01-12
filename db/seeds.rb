43e2f8376af407262a17b8e7dfdb74645179dbba
Scott Macfarlane <scott@gina.alaska.edu>
2016-01-11 15:29:09 -0900

    Add seeds for AWRVI 2015 categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def create_category(seed_data)
+  Category.where(name: seed_data['name']).first_or_create(category_params(seed_data)) do |c|
+    c.children = create_sub_categories(seed_data) if seed_data.has_key?('categories')
+  end
+end
+
+def create_sub_categories(seed_data)
+  seed_data['categories'].map do |sub_c|
+    create_category(sub_c)
+  end
+end
+
+def category_params(category)
+  category.select{|k,v| %w{short_name order}.include?(k)}
+end
+
+Dir.glob(File.join(File.dirname(__FILE__), "seeds", "categories", "*.yaml")).each do |seed|
+  puts "Loading #{seed}"
+  create_category(YAML.load_file(seed))
+end
+
