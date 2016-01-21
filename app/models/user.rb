8f7927bc56cd00d3345117bb347b81b8c1ddf68f
Scott Macfarlane <scott@gina.alaska.edu>
2016-01-20 16:54:24 -0900

    Wire up openid in devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :registerable, :recoverable, :rememberable, :trackable,
-         :validatable, :omniauthable, :confirmable
-
-         #Removed :database_authenticatable,
+         :validatable, :confirmable, :omniauthable, omniauth_providers: [:open_id]
+         :database_authenticatable
 
 end
