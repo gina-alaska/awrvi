82b6516fc372b9578c72b0d9608a1143932a1e9e
Scott Macfarlane <scott@gina.alaska.edu>
2016-01-25 10:08:43 -0900

    Remove commented code

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  skip_before_filter :verify_authenticity_token#, :only => :create
+  skip_before_filter :verify_authenticity_token
 
   def open_id
     @user = User.from_omniauth(request.env["omniauth.auth"])
