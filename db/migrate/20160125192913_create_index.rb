c06ae029b6501ffd906584d73831dad7e09eca12
Grimm <jason@gina.alaska.edu>
2016-01-25 10:43:18 -0900

    Changed how the indices table is generated using references now.

--- db/migrate/20160125192913_create_index.rb
+++ db/migrate/20160125192913_create_index.rb
@@ -1,13 +1,11 @@
-class CreateIndices < ActiveRecord::Migration[5.0]
+class CreateIndex < ActiveRecord::Migration[5.0]
   def change
     create_table :indices do |t|
       t.datetime :finalized_at
-      t.integer :category_id
+      t.references :category, index: true, foreign_key: true
       t.decimal :awrvi_index, precision: 6, scale: 5
       t.datetime :rejected_at
       t.text :rejected_reason
-
-      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114011126) do
+ActiveRecord::Schema.define(version: 20160125192913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,15 +53,22 @@ ActiveRecord::Schema.define(version: 20160114011126) do
     t.index ["slug"], name: "index_communities_on_slug", unique: true, using: :btree
   end
 
+  create_table "index_choices", force: :cascade do |t|
+    t.integer  "index_id"
+    t.integer  "choice_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "indices", force: :cascade do |t|
     t.datetime "finalized_at"
     t.integer  "category_id"
     t.decimal  "awrvi_index",     precision: 6, scale: 5
     t.datetime "rejected_at"
     t.text     "rejected_reason"
-    t.datetime "created_at",                              null: false
-    t.datetime "updated_at",                              null: false
+    t.index ["category_id"], name: "index_indices_on_category_id", using: :btree
   end
 
   add_foreign_key "choices", "categories"
+  add_foreign_key "indices", "categories"
 end
