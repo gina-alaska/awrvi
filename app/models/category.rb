3434d286c066e860e39620a844434c8ece13af7d
Grimm <jason@gina.alaska.edu>
2016-01-26 09:25:43 -0900

    Added category and index associations and made sure that categories can not be destroyed.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,11 @@
 class Category < ApplicationRecord
   has_closure_tree order: 'position'
+  before_destroy :no_destroy
 
   has_many :choices, -> { order(index: :desc) }, dependent: :destroy
+  has_many :indices
+
+  def no_destroy
+    false
+  end
 end
