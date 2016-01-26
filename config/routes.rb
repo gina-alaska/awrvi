b7da44de29c22bb51eb8db4a5ea84101dec0b5c5
Grimm <jason@gina.alaska.edu>
2016-01-26 09:24:35 -0900

    Adjusted the categories route to remove the destroy action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :indices
   resources :choices
-  resources :categories
+  resources :categories, except: :destroy
   resources :communities
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
