2d108a3b48ae69e756d235616c30da769d3eed5b
Grimm <jason@gina.alaska.edu>
2016-01-27 10:33:30 -0900

    Generated new migration for index_choices.

--- db/migrate/20160127193036_create_new_index_choices.rb
+++ db/migrate/20160127193036_create_new_index_choices.rb
@@ -1,4 +1,4 @@
-class CreateIndexChoices < ActiveRecord::Migration[5.0]
+class CreateNewIndexChoices < ActiveRecord::Migration[5.0]
   def change
     create_table :index_choices do |t|
       t.references :index, index: true, foreign_key: true
