8944b17fdb24e252f424ae2ca30d205fc0c818da
Scott Macfarlane <scott@gina.alaska.edu>
2016-01-28 14:49:50 -0900

    Set null: false on indices/awrvi_version_id
    You can't have an index without it pointing at an AWRVI version. This adds
    some safety to ensure we're always linking the two together.
    
    Add not-null constraint to index/awrvi_version_id
    
    Add awrvi_version in fixtures
    
    Include awrvi_version in controller test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127193036) do
+ActiveRecord::Schema.define(version: 20160128235605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 20160127193036) do
 
   create_table "indices", force: :cascade do |t|
     t.datetime "finalized_at"
-    t.integer  "awrvi_version_id"
+    t.integer  "awrvi_version_id",                         null: false
     t.integer  "community_id"
     t.decimal  "awrvi_index",      precision: 6, scale: 5
     t.datetime "rejected_at"
