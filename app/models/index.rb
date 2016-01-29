b7bc2583250775d907856f8d608275adca48f483
Scott Macfarlane <scott@gina.alaska.edu>
2016-01-29 14:45:22 -0900

    First bad pass at forms

--- app/models/index.rb
+++ app/models/index.rb
@@ -1,9 +1,8 @@
 class Index < ApplicationRecord
   belongs_to :community
   belongs_to :awrvi_version, class_name: Category
-  belongs_to :category
-  has_many :index_category_choices, dependent: :destroy
-  has_many :choices, through: :index_category_choices
+  has_many :index_choices, dependent: :destroy
+  has_many :choices, through: :index_choices
 
   accepts_nested_attributes_for :choices
 end
