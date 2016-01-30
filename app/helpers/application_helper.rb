d187cb667f48bdb4a4de1e933986eb76fb7ea117
Scott Macfarlane <scott@gina.alaska.edu>
2016-01-29 17:13:04 -0900

    Make choice selection work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,10 @@ module ApplicationHelper
       'info'
     end
   end
+
+  def category_choices(index, category)
+    icc = index.index_category_choices.select{|icc| icc.category_id == category.id }
+
+    icc.first || index.index_category_choices.build(category: category)
+  end
 end
