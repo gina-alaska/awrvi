cc56c5e4233bcb75a3ec0a5de52e9b45799092e9
Scott Macfarlane <scott@gina.alaska.edu>
2016-01-29 15:05:19 -0900

    Rename IndexChoices to IndexCategoryChoices

--- app/models/index.rb
+++ app/models/index.rb
@@ -1,6 +1,8 @@
 class Index < ApplicationRecord
   belongs_to :community
   belongs_to :category
-  has_many :index_choices, dependent: :destroy
-  has_many :choices, through: :index_choices
+  has_many :index_category_choices, dependent: :destroy
+  has_many :choices, through: :index_category_choices
+
+  accepts_nested_attributes_for :choices
 end
