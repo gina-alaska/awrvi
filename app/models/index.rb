250053ce9047772803ed0fdb7596be1cefc5feb8
Scott Macfarlane <scott@gina.alaska.edu>
2016-02-08 13:49:08 -0900

    Fix oops, didn't save before commit

--- app/models/index.rb
+++ app/models/index.rb
@@ -1,10 +1,7 @@
 class Index < ApplicationRecord
   belongs_to :community
-<<<<<<< HEAD
   belongs_to :awrvi_version, class_name: Category
-=======
   belongs_to :category
->>>>>>> cc56c5e4233bcb75a3ec0a5de52e9b45799092e9
   has_many :index_category_choices, dependent: :destroy
   has_many :choices, through: :index_category_choices
 
