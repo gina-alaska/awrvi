d7b46389064cf0d76dba3ae4c088835f82857458
Scott Macfarlane <scott@gina.alaska.edu>
2016-02-08 09:16:22 -0900

    Add test for entering data into index form

--- test/integration/indices_forms_test.rb
+++ test/integration/indices_forms_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class IndicesFormsTest < ActionDispatch::IntegrationTest
   test 'a user can create an index' do
-    visit(new_index_url)
+    choice = choices(:med)
+    visit(new_index_path)
 
+    select(choice.description, from: choice.category.name)
+    click_on('Create Index')
+
+    assert page.has_content?("#{choice.category.name}: #{choice.index}")
   end
 end
