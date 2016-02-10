51fa1c0cce3d59bd3c528ec5337a9d659b3d3040
Scott Macfarlane <scott@gina.alaska.edu>
2016-02-10 10:53:21 -0900

    Add tests for what user abilities

---
+++ test/models/ability_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class AbilityTest < ActiveSupport::TestCase
+  def test_users_can_only_read_themselves
+    user = users(:one)
+    ability = Ability.new(user)
+    assert ability.can?(:read, users(:one)), 'User cannot read themself'
+    assert ability.can?(:update, users(:one)), 'User cannot update themself'
+    assert ability.cannot?(:read, User.new), 'User can read other users'
+  end
+
+  def test_admins_can_manage_all_users
+    user = users(:one)
+    user.user_admin = true
+    ability = Ability.new(user)
+
+    assert ability.can?(:manage, User.new), 'User Admin cannot manage users'
+  end
+
+end
