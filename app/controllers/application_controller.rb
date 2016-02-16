b6939a6abb8a4b51c7765ad087ea3960962daf85
Scott Macfarlane <scott@gina.alaska.edu>
2016-02-16 14:27:20 -0900

    Fix rubocop complain from copy/paste

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :search_params
-  check_authorization
+  check_authorization unless: :devise_controller?
 
   private
 
