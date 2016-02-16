6ee717d37f160befe2dc21190c5067984e3d03b1
Scott Macfarlane <scott@gina.alaska.edu>
2016-02-16 11:47:46 -0900

    Update test to use correct strings

--- test/integration/user_management_test.rb
+++ test/integration/user_management_test.rb
@@ -2,15 +2,16 @@ require 'test_helper'
 
 class UserManagementTest < ActionDispatch::IntegrationTest
   def test_admins_can_edit_users
+    target_user = users(:one)
     login(:admin)
     visit manage_users_path
-    assert page.has_content?('User One'), 'Users not listed on page'
+    assert page.has_content?(target_user.name), 'Users not listed on page'
 
-    click_on('User One edit link')
-    check('Can Manage Users')
-    click_button("Update User")
+    click_on("manage_user_#{target_user.id}")
+    check('User admin')
+    click_button("Update user")
 
-    assert page.has_content?("User One updated")
+    assert page.has_content?("User was successfully updated.")
   end
 
 end
