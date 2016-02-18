b6eac779fb77a63189e2a6aa557577e413ed74dd
Scott Macfarlane <scott@gina.alaska.edu>
2016-02-18 12:13:14 -0900

    Refactor tests to group by scenario

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,64 +1,74 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = users(:one)
+module ManageUserControllerTests
+  class UserAdminTest < ActionDispatch::IntegrationTest
+    setup do
+      login_as(users(:admin))
+      @user = users(:one)
+    end
+
+    teardown do
+      Warden.test_reset!
+    end
+
+    test "should get index" do
+      get manage_users_url
+      assert_response :success
+    end
+
+    test 'manage interface sets the right variant' do
+      login_as(users(:admin), scope: :user)
+      get manage_user_url(@user)
+      assert_includes controller.request.variant, :'user-admin'
+    end
+
+    test 'admins can update user abilities' do
+      login_as(users(:admin), scope: :user)
+      patch manage_user_url(@user), params: { user: {user_admin: true } }
+      assert_redirected_to manage_user_path(@user)
+    end
   end
 
-  teardown do
-    Warden.test_reset!
-  end
 
-  test "should get index" do
-    get manage_users_url
-    assert_response :success
-  end
+  class UserProfileTest < ActionDispatch::IntegrationTest
+    setup do
+      @user = users(:one)
+    end
+    
+    test "can't view other users" do
+      assert_raises CanCan::AccessDenied do
+        get profile_url(@user)
+      end
+    end
 
-  test "can't view other users" do
-    assert_raises CanCan::AccessDenied do
+    test 'users can view themsleves' do
+      login_as(@user)
       get profile_url(@user)
+      assert_response :success
     end
-  end
 
-  test 'users can view themsleves' do
-    login_as(@user)
-    get profile_url(@user)
-    assert_response :success
-  end
-
-  test "can't edit other users" do
-    assert_raises CanCan::AccessDenied do
-      get edit_manage_user_url(@user)
+    test "can't edit other users" do
+      assert_raises CanCan::AccessDenied do
+        get edit_manage_user_url(@user)
+      end
     end
-  end
 
-  test 'can edit themselves' do
-    login_as(@user)
-    get profile_url(@user)
-    assert_response :success
-  end
-
-  test "should update user" do
-    assert_raises CanCan::AccessDenied do
-      patch manage_user_url(@user), params: { user: { user_admin: true } }
+    test 'can edit themselves' do
+      login_as(@user)
+      get profile_url(@user)
+      assert_response :success
     end
-  end
 
-  test "should destroy user" do
-    assert_raises CanCan::AccessDenied do
-      delete manage_user_url(@user)
+    test "should update user" do
+      assert_raises CanCan::AccessDenied do
+        patch manage_user_url(@user), params: { user: { user_admin: true } }
+      end
     end
-  end
 
-  test 'manage interface sets the right variant' do
-    login_as(users(:admin), scope: :user)
-    get manage_user_url(@user)
-    assert_includes controller.request.variant, :'user-admin'
-  end
-
-  test 'admins can update user abilities' do
-    login_as(users(:admin), scope: :user)
-    patch manage_user_url(@user), params: { user: {user_admin: true } }
-    assert_redirected_to manage_user_path(@user)
+    test "should destroy user" do
+      assert_raises CanCan::AccessDenied do
+        delete manage_user_url(@user)
+      end
+    end
   end
 end
