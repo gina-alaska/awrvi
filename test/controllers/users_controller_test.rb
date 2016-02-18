656ab76a58b3cf7deef1958e80df0800d19b7f79
Scott Macfarlane <scott@gina.alaska.edu>
2016-02-18 10:41:33 -0900

    Add tests to test different behaviors for UserController

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,21 +5,37 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @user = users(:one)
   end
 
+  teardown do
+    Warden.test_reset!
+  end
+
   test "should get index" do
     get manage_users_url
     assert_response :success
   end
 
-  test "should show user" do
+  test "can't view other users" do
     assert_raises CanCan::AccessDenied do
-      get manage_user_url(@user)
+      get profile_url(@user)
     end
   end
 
-  test "should get edit" do
+  test 'users can view themsleves' do
+    login_as(@user)
+    get profile_url(@user)
+    assert_response :success
+  end
+
+  test "can't edit other users" do
     assert_raises CanCan::AccessDenied do
       get edit_manage_user_url(@user)
     end
+  end
+
+  test 'can edit themselves' do
+    login_as(@user)
+    get profile_url(@user)
+    assert_response :success
   end
 
   test "should update user" do
@@ -32,5 +48,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_raises CanCan::AccessDenied do
       delete manage_user_url(@user)
     end
+  end
+
+  test 'manage interface sets the right variant' do
+    login_as(users(:admin), scope: :user)
+    get manage_user_url(@user)
+    assert_includes controller.request.variant, :'user-admin'
+  end
+
+  test 'admins can update user abilities' do
+    login_as(users(:admin), scope: :user)
+    patch manage_user_url(@user), params: { user: {user_admin: true } }
+    assert_redirected_to manage_user_path(@user)
   end
 end
