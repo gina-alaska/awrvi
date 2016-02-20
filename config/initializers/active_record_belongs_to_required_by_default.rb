dbd10b4063f096b94782875cf16e8f23b602dc51
Scott Macfarlane <scott@gina.alaska.edu>
2016-02-19 17:06:52 -0900

    Fix friendly_id #73

--- config/initializers/active_record_belongs_to_required_by_default.rb
+++ config/initializers/active_record_belongs_to_required_by_default.rb
@@ -1,5 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Require `belongs_to` associations by default. This is a new Rails 5.0 default, 
+# Require `belongs_to` associations by default. This is a new Rails 5.0 default,
 # so introduced as a config to ensure apps made with earlier versions of Rails aren't affected when upgrading.
-Rails.application.config.active_record.belongs_to_required_by_default = true
+
+# Fix for Friendly_ID #73
+# https://github.com/norman/friendly_id/issues/730
+ActiveRecord::Base.belongs_to_required_by_default = true
