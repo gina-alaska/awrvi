83956952862ab24aaa8906e291e445b3b5376fca
Scott Macfarlane <scott@gina.alaska.edu>
2016-02-22 08:33:31 -0900

    Revert to rails4 behavior

--- config/initializers/active_record_belongs_to_required_by_default.rb
+++ config/initializers/active_record_belongs_to_required_by_default.rb
@@ -3,6 +3,4 @@
 # Require `belongs_to` associations by default. This is a new Rails 5.0 default,
 # so introduced as a config to ensure apps made with earlier versions of Rails aren't affected when upgrading.
 
-# Fix for Friendly_ID #73
-# https://github.com/norman/friendly_id/issues/730
-ActiveRecord::Base.belongs_to_required_by_default = true
+Rails.application.config.active_record.belongs_to_required_by_default = true
