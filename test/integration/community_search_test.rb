506214787bd69daf7af3124a4a6a33d2999ca372
Grimm <jason@gina.alaska.edu>
2016-02-23 15:03:19 -0900

    Added integration test for community search and fleshed out the fixture.

--- test/integration/community_search_test.rb
+++ test/integration/community_search_test.rb
@@ -2,7 +2,21 @@ require 'test_helper'
 
 class CommunitySearchTest < ActionDispatch::IntegrationTest
   test 'Search for one community' do
-    get '/welcome'
-    assert_response :success
+    community = communities(:one)
+
+    visit('/welcome')
+    fill_in('q', with: community.name)
+    click_button('search')
+
+    assert page.has_content?("Name: #{community.name}")
+  end
+
+  test 'Search for multiple communities' do
+    visit('/welcome')
+    fill_in('q', with: 'Test')
+    click_button('search')
+
+    assert page.has_content?("Test")
+    assert page.has_content?("Testing")
   end
 end
