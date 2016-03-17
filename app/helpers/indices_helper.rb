681fe21938c2876b46aa8e13515bba7644221826
Scott Macfarlane <scott@gina.alaska.edu>
2016-03-16 17:24:51 -0800

    Fix rubocop offenses

--- app/helpers/indices_helper.rb
+++ app/helpers/indices_helper.rb
@@ -1,11 +1,11 @@
 module IndicesHelper
-  def indicator_percentage index, indicator
+  def indicator_percentage(index, indicator)
     leaves = indicator.leaves.pluck(:id)
     percentage = index.choices.where(category: leaves).sum(:index) / leaves.count
     percentage * 100
   end
 
-  def progress_bar_for_percentage percentage
+  def progress_bar_for_percentage(percentage)
     case
     when percentage >= 60
       'progress-bar-success'
