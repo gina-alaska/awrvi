08787e57f1b41ea13191a06f951ea9c48198029f
Grimm <jason@gina.alaska.edu>
2016-03-21 11:05:49 -0800

    Reorganized the ability model test so that it only has three methods for guest, user and admin.

--- test/models/ability_test.rb
+++ test/models/ability_test.rb
@@ -1,67 +1,52 @@
 require 'test_helper'
 
 class AbilityTest < ActiveSupport::TestCase
-  def test_users_can_only_read_themselves
-    user = users(:one)
-    ability = Ability.new(user)
-    assert ability.can?(:read, users(:one)), 'User cannot read themself'
-    assert ability.can?(:update, users(:one)), 'User cannot update themself'
-    assert ability.cannot?(:read, users(:two)), 'User can read other users'
-  end
-
-  def test_admins_can_manage_all_users
-    user = users(:one)
-    user.user_admin = true
-    ability = Ability.new(user)
-
-    assert ability.can?(:manage, User.new), 'User Admin cannot manage users'
-  end
-
-  def test_users_cannot_create_choices
-    user = users(:one)
-    ability = Ability.new(user)
-
-    assert ability.cannot?(:create, Choice.new), "User can create a choice"
-    assert ability.cannot?(:read, Choice.new), "User can read a choice"
-    assert ability.cannot?(:update, Choice.new), "User can update a choice"
-  end
-
-  def test_only_category_admin_can_edit
-    user = users(:category_admin)
-    ability = Ability.new(user)
-
-    assert ability.can?(:manage, choices(:med)), "Category admin cannot manage choices"
-    assert ability.can?(:edit, choices(:low)), "category_admin cannot edit choices"
-  end
-
-  def test_users_can_create_indices
-    user = users(:one)
-    ability = Ability.new(user)
-
-    assert ability.can?(:create, Index.new), "User cannot create index"
-  end
-
-  def test_guests_can_only_read_indicies
+  def test_guests
     user = User.new
     ability = Ability.new(user)
 
     assert ability.can?(:read, indices(:one)), "Guest cannot read index"
     assert ability.cannot?(:create, Index.new), "Guest can create Index"
+    assert ability.cannot?(:edit, indices(:one)), "Guest can edit Index"
+    assert ability.cannot?(:destroy, indices(:one)), "Guest can delete Index"
   end
 
-  def test_only_indicies_owner_can_edit
+  def test_users
     user = users(:one)
     ability = Ability.new(user)
+
+    assert ability.can?(:read, users(:one)), 'User cannot read themself'
+    assert ability.can?(:update, users(:one)), 'User cannot update themself'
+    assert ability.cannot?(:read, users(:two)), 'User can read other users'
+
+    assert ability.cannot?(:create, Choice.new), "User can create a choice"
+    assert ability.cannot?(:read, Choice.new), "User can read a choice"
+    assert ability.cannot?(:update, Choice.new), "User can update a choice"
+
+    assert ability.can?(:create, Index.new), "User cannot create index"
 
     assert ability.can?(:manage, indices(:one)), "User cannot manage their index"
     assert ability.cannot?(:edit, indices(:two)), "User can edit index they don't own"
-  end
-
-  def test_only_indicies_owner_can_delete
-    user = users(:one)
-    ability = Ability.new(user)
-
     assert ability.can?(:destroy, indices(:unfinalized)), "User cannot destroy their unfinalized index"
     assert ability.cannot?(:destroy, indices(:two)), "User can destroy an index they don't own"
+  end
+
+  def test_admins
+    user = users(:category_admin)
+    category_ability = Ability.new(user)
+
+    user = users(:one)
+    user.user_admin = true
+    user_ability = Ability.new(user)
+
+    assert category_ability.can?(:create, Choice.new), "category_admin cannot create choices"
+    assert category_ability.can?(:read, choices(:low)), "category_admin cannot read choices"
+    assert category_ability.can?(:edit, choices(:low)), "category_admin cannot edit choices"
+    assert category_ability.can?(:destroy, choices(:low)), "category_admin cannot delete choices"
+
+    assert user_ability.can?(:create, User.new), 'User Admin cannot create users'
+    assert user_ability.can?(:read, users(:one)), 'User Admin cannot create users'
+    assert user_ability.can?(:edit, users(:one)), 'User Admin cannot edit users'
+    assert user_ability.can?(:destory, users(:one)), 'User Admin cannot delete users'
   end
 end
