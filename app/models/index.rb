4aafad0e2d92e19bf228fcce91246f0055871a37
Scott Macfarlane <scott@gina.alaska.edu>
2016-03-29 12:31:17 -0800

    Validate hidden_reason is present when Index is hidden

--- app/models/index.rb
+++ app/models/index.rb
@@ -9,6 +9,9 @@ class Index < ApplicationRecord
   accepts_nested_attributes_for :index_category_choices
 
   validates :community_id, presence: true
+  validates :hidden_reason, presence: {message: "Reason for hiding Index must be specified"},
+              if: ->(i) { i.hidden? }
+
 
   def completeness
     "#{choices.count} / #{awrvi_version.leaves.count}"
