a216d0c837b589a1253b31c39b97c3fbc83e66ee
Will Fisher <will@gina.alaska.edu>
2016-03-30 14:45:11 -0800

    adding helpers for finding dashboards for users

--- app/helpers/communities_helper.rb
+++ app/helpers/communities_helper.rb
@@ -2,4 +2,17 @@ module CommunitiesHelper
   def sidebar_css(truthy)
     truthy ? 'with-sidebar' : 'without-sidebar'
   end
+
+  def latest_awrvi_dashboard(community)
+    published_dashboards(community).recent.first
+  end
+
+  def published_dashboards(community, opts = {})
+    indices = community.indices.published
+    if opts.include?(:exclude)
+      indices = indices.where.not(id: opts[:exclude])
+    end
+
+    indices
+  end
 end
