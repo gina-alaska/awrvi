9b3f8f2ea33e6fc5ae4ee1b73d5bfc29106647bd
Will Fisher <will@gina.alaska.edu>
2016-03-30 14:42:52 -0800

    add handling for showing locations and static maps

--- app/models/community.rb
+++ app/models/community.rb
@@ -11,4 +11,32 @@ class Community < ApplicationRecord
   def to_s
     name
   end
+
+  def latlng
+    [geometry.envelope.center.lat, geometry.envelope.center.lng]
+  end
+  def lnglat
+    [geometry.envelope.center.lng, geometry.envelope.center.lat]
+  end
+
+  def geometry
+    GeoRuby::SimpleFeatures::Geometry.from_ewkt(self.location)
+  end
+
+  def location?
+    !location.blank? && geometry
+  end
+
+  def static_map_url(zoom = 11, size = '300x300')
+    File.join(
+      'https://api.mapbox.com/v4/mapbox.streets',
+      "pin-s-marker+f44(#{lnglat.join(',')})",
+      "#{lnglat.join(',')},#{zoom}",
+      "#{size}.png"
+    ) + "?access_token=#{Rails.application.secrets.mapbox_api_token}"
+  end
+
+  def static_map_attribution
+    "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>".html_safe
+  end
 end
