47588344f84ee8596bbb06129bd57af198666407
Will Fisher <will@gina.alaska.edu>
2016-04-06 12:06:52 -0800

    sort most vulnerable first

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   has_closure_tree order: 'position'
   before_destroy :no_destroy
 
-  has_many :choices, -> { order(index: :desc) }, dependent: :destroy
+  has_many :choices, -> { order(index: :asc) }, dependent: :destroy
   has_many :indices
 
   def no_destroy
