3810092375f2bd23af134afab981d4b9e16bff0d
Will Fisher <will@gina.alaska.edu>
2016-04-27 17:13:29 -0800

    fix showing communities that don't have indices

--- app/controllers/concerns/index_review_concerns.rb
+++ app/controllers/concerns/index_review_concerns.rb
@@ -2,7 +2,9 @@ module IndexReviewConcerns
   extend ActiveSupport::Concern
 
   def fetch_users_review_for_index
-    @review = @index.reviews.accessible_by(current_ability, :manage).first
-    @review ||= @index.reviews.build
+    if @index
+      @review = @index.reviews.accessible_by(current_ability, :manage).first
+      @review ||= @index.reviews.build
+    end
   end
 end
